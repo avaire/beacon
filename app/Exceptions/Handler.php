1aea9d56c11a5c44f08ac3c0dea63e9d519da452
Alexis Tan <alexis@sen-dev.com>
2018-07-03 16:30:49 +0200

    Add fallback 404 error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +46,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof NotFoundHttpException) {
+            return response()->json([
+                'status' => 404,
+                'reason' => 'Route was not found!'
+            ], 404);
+        }
+
         return parent::render($request, $e);
     }
 }
